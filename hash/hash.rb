55e08b80c2e34417c2d8a7f24fd088833e816842
pedrocauan <mainyasuo92@gmail.com>
2023-03-02 14:02:03 -0300

    hashes e metodos para hashes

---
+++ hash/hash.rb
@@ -0,0 +1,19 @@
+# hash são como dicionarios
+
+hash = {nome: "pedro", idade: 23}
+hash[:altura] = 1.73
+# hash.delete(:altura) # deleta a chave
+
+# hash.clear #apaga toda a hash
+# puts hash.has_value?("pedro") # ve se tem um valor dentro da hash
+# puts hash.has_key?(:nome) # procura a chave
+# puts hash.keys # mostra as chaves que existem dentro da hash
+# puts hash.values # mostra os valores das chaves
+# puts hash.size # tamanho da hash
+# puts hash.length  # tamanho da hash
+puts hash
+
+# hash1 = { :um => 1, :dois => 2, :tres => 3}
+# hash2 = { :quatro => 1, :cinco => 2, :seis => 3}
+
+

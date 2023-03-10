d45a74e13bd73525bedd58cb49a39a05a0154fbf
pedrocauan <mainyasuo92@gmail.com>
2023-03-10 17:08:46 -0300

    unless'

---
+++ condicionais/unless.rb
@@ -0,0 +1,12 @@
+# unless executa um if reverso kkk
+
+x = 10
+
+# executa se a expressao é falsa
+unless x > 15
+    puts "x é menor que 15"
+
+#executa se a expressao verdadeira
+else
+    puts "x é maior que 15"
+end

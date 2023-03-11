72dd3c67ee4d08a87cb246157b4f6f36650c0c7c
pedrocauan <mainyasuo92@gmail.com>
2023-03-11 12:52:26 -0300

    modify

---
+++ repeticao/for.rb
@@ -0,0 +1,11 @@
+nomes = ["pedro", "maria", "clara", "joana"]
+
+dict = {nome: "Mathias", idade: 35, altura: 2.10}
+
+# for nome in nomes do
+#     puts nome
+# end
+
+for k, v in dict do
+    puts "#{k}: #{v}"
+end

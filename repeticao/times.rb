bf1903bd55890563ebe679e5c68dbaf62f1be5f1
pedrocauan <mainyasuo92@gmail.com>
2023-03-11 13:01:24 -0300

    times'

---
+++ repeticao/times.rb
@@ -0,0 +1,30 @@
+
+# repete n vezes o que ta dentro do bloco
+#c = 0
+# 10.times do
+#     puts "___________________________"
+#     puts c
+#     c += 1
+# end
+
+#
+
+nomes = ["pedro", "maria", "clara", "joana"]
+
+i = 0
+
+nomes.size.times do
+    puts "#{i+1}: #{nomes[i]}"
+    i += 1
+end
+
+
+
+qnt_registros = 10
+
+
+count = 1
+qnt_registros.times do 
+    puts "registro #{count}"
+    count += 1
+end

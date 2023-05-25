7ef62f01dadae5984cb4f35307e938db9f05ab45
pedrocauan <mainyasuo92@gmail.com>
2023-05-25 20:38:24 -0300

    brincando

---
+++ ex/ano_decada_segundos.rb
@@ -0,0 +1,9 @@
+# quantas horas tem em um ano?
+puts 
+print "Em um ano tem ",  (1 * 24 * 31 * 12) - (24 * 4) - (24 * 3), " horas\n"
+
+# Quantos minutos há em uma decada?
+print "Em uma decada tem ", ((24 * 31 * 12) - (24 * 4) - (24 * 3)) * 60 * 10 , " minutos\n0"
+
+# Qual a sua idade em segundos?
+print 22 , " anos em segundos é " , ((24 * 31 * 12) - (24 * 4) - (24 * 3)) * 60 * 60 * 22 

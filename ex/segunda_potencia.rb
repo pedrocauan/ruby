b117dbf6ab97cb802ad5e261d11b17b183816e94
pedrocauan <mainyasuo92@gmail.com>
2023-06-10 22:57:12 -0300

    segunda potencia

---
+++ ex/segunda_potencia.rb
@@ -0,0 +1,18 @@
+# Utilizando uma colletion do tipo array, escreva  um programa que recebe 3 números e no 
+# final exiba o resultado de cada um deles  elevado a segunda potencia
+
+numbers = Array.new
+
+# Pegando os numeros
+#3.times do numbers.push(gets.chomp.to_i) end
+#numbers.each do | number | puts "#{number ** 2}" end
+#for i in 3.times do numbers.push(gets.chomp.to_i) end
+
+#for i in 3.times do
+#	numbers[i] = gets.chomp.to_i
+#	numbers[i] = numbers[i].pow(2)
+#end
+
+puts numbers
+
+

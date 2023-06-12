d122da01854bcdea978980cf763e1ef0b16b9b10
pedrocauan <mainyasuo92@gmail.com>
2023-06-11 22:42:05 -0300

    gems, bundle e gemfile

---
+++ ex/potencia.rb
@@ -0,0 +1,9 @@
+# Crie um programa  que possua um método  que resolva a 
+# Potencia dado um numero  base e seu expoente. Estes dois
+# valores devem ser  informádos  pelo usuario
+
+def potencia(b,e)
+    b**e
+end
+
+puts potencia(2,3)
